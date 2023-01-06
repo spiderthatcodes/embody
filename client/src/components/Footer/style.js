b2a6bc93555582197308e08db3ab85126c44053d
spiderthatcodes <75698869+jdickerson-banyan@users.noreply.github.com>
2023-01-06 12:15:00 -0700

    footer positioning

--- client/src/components/Footer/style.js
+++ client/src/components/Footer/style.js
@@ -1,3 +1,8 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    padding: 80px;
+`;
