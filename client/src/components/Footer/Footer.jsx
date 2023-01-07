05bc45de6f1856bd7c6a31d2d6c77d4ea86f5cbf
spiderthatcodes <75698869+jdickerson-banyan@users.noreply.github.com>
2023-01-07 13:25:35 -0700

    added most content to footer, still need pics

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,15 +1,34 @@
-import { useLocation } from 'react-router-dom';
-import { Container, EmailBar } from './style';
+import { useLocation, useNavigate, Link } from 'react-router-dom';
+import {
+    Container,
+    EmailBar,
+    LogoContainer,
+    Title,
+    Subtitle,
+    LinkContainer,
+    NavRow,
+} from './style';
 import LeafPanel from '../LeafPanel/LeafPanel';
 
 const Footer = () => {
     const location = useLocation();
-
+    const navigate = useNavigate();
 
     return (
         <Container>
             <LeafPanel page={location.pathname} />
-            <div id='contact-bar'></div>
+            <NavRow>
+                <LogoContainer onClick={() => navigate('/')}>
+                    <Title>Embody</Title>
+                    <Subtitle>Coaching & Counseling</Subtitle>
+                </LogoContainer>
+                <LinkContainer>
+                    <Link to='/about'>About</Link>
+                    <Link to='/services'>Services</Link>
+                    <Link to='/blog'>Blog</Link>
+                    <Link to='/contact'>Contact</Link>
+                </LinkContainer>
+            </NavRow>
             <EmailBar>
                 <p>Personalized coaching & counseling</p>
                 <p>embody@gmail.com</p>
--- client/src/components/Footer/style.js
+++ client/src/components/Footer/style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { colors } from '../../theme';
 
 export const Container = styled.div`
     position: fixed;
@@ -12,4 +13,38 @@ export const EmailBar = styled.div`
     display: flex;
     justify-content: space-between;
     padding: 20px 80px;
+`;
+
+export const LinkContainer = styled.div`
+    display: flex;
+    width: 300px;
+    justify-content: space-between;
+    a {
+        text-decoration: none;
+        color: ${colors.black};
+    }
+`;
+
+export const Title = styled.h1`
+    font-size: 55px;
+    line-height: 1px;
+    color: ${colors.black};
+`;
+
+export const Subtitle = styled.p`
+    font-size: 14px;
+    line-height: 1px;
+    color: ${colors.black};
+`;
+
+export const LogoContainer = styled.div`
+    width: 100%;
+    margin: auto;
+    text-align: left;
+`;
+
+export const NavRow = styled.div`
+    display: flex;
+    padding: 20px 80px;
+    align-items: center;
 `;
