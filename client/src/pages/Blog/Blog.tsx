ca0bd64fb5ee3c59bd1e093b77ef04225f0f4aa3
spiderthatcodes <75698869+jdickerson-banyan@users.noreply.github.com>
2023-02-24 13:34:53 -0700

    pulling from redux

--- client/src/pages/Blog/Blog.tsx
+++ client/src/pages/Blog/Blog.tsx
@@ -1,10 +1,15 @@
 import { FC } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../app/store';
 import Layout from '../../components/Layout/Layout';
 
 const Blog: FC = () => {
+    const blogs = useSelector((store: RootState) => store.blogs.blogs);
+    console.log(blogs)
+
     return (
         <Layout>
-            <div>Blog</div>
+            
         </Layout>
     );
 };
