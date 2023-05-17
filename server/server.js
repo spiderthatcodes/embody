42d9ff60d618c536c55d3020ca2e6efe008a40d2
spiderthatcodes <75698869+jdickerson-banyan@users.noreply.github.com>
2023-05-17 13:31:09 -0700

    adding security to the backend

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
+const helmet = require('helmet')
+const xssClean = require('xss-clean');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const cors = require('cors');
@@ -11,6 +13,9 @@ connectDB();
 
 const app = express();
 app.use(cors());
+
+app.use(helmet());
+app.use(xssClean());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
